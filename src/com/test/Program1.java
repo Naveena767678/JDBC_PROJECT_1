703090f3994824b25bfe4ff4a41e7af1f7ed5024
Naveena <naveenae19cs029@gmail.com>
2025-05-06 09:38:23 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -5,10 +5,10 @@ public class Program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("hello world!");
-		Scanner sc=new Scanner(system.in);
+		Scanner sc=new Scanner(System.in);
 		 System.out.println("square number");
 		 System.out.println("enter the number:");
-		int n sc.nextInt();
+		int n= sc.nextInt();
 		int sqnum =n*n;
 		 System.out.println("the square number of "+n+"is "+sqnum);
 		
