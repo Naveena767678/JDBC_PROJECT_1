fd04e086a823d617562c52bd8b8fef37024ccbeb
Naveena <naveenae19cs029@gmail.com>
2025-05-06 09:32:03 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -1,10 +1,17 @@
 package com.test;
-
+import java.util.Scanner;
 public class Program1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hello world");
+        System.out.println("hello world!");
+		Scanner sc=new Scanner(system.in);
+		 System.out.println("square number");
+		 System.out.println("enter the number:");
+		int n sc.nextInt();
+		int sqnum =n*n;
+		 System.out.println("the square number of "+n+"is "+sqnum);
+		
 	}
 
 }
